fa695619b0001e5c257fed36e6a7990cfb874649
Mark Johnson <markjohnson303@gmail.com>
2018-04-10 09:39:50 -0600

    removed auth0

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,20 +2,6 @@
 
   <div id="app">
     <div class="uk-container">
-      <h1>Auth Flow</h1>
-      <ul>
-        <li>
-          <router-link v-if="loggedIn" to="/logout">Log out</router-link>
-          <router-link v-if="!loggedIn" to="/login">Log in</router-link>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-        <li>
-          <router-link to="/dashboard">Dashboard</router-link>
-          (authenticated)
-        </li>
-      </ul>
       <div class="uk-card uk-card-body uk-card-primary">
         <h3 class="uk-card-title">Example headline</h3>
 
@@ -29,21 +15,10 @@
 <script>
 import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
-import auth from './auth'
 
 UIkit.use(Icons)
 export default {
-  name: 'App',
-  data () {
-    return {
-      loggedIn: auth.loggedIn()
-    }
-  },
-  created () {
-    auth.onChange = loggedIn => {
-      this.loggedIn = loggedIn
-    }
-  }
+  name: 'App'
 }
 </script>
 
